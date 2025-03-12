512d8c83fcfb897ca7ea71a288b9dc6fe4eaa00f
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-03-12 21:57:50 +0300

    Implement session management with MongoDB and configure CORS for local development

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,11 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 
+import { buildContext } from 'graphql-passport'
+import passport from 'passport';
+import session from 'express-session';
+import ConnectMongoDBSession from 'connect-mongodb-session';
+
 import mergedResolver from "./resolver/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 import { connectDb } from './db/connectDB.js';
@@ -13,6 +18,28 @@ import { connectDb } from './db/connectDB.js';
 const app = express()
 const httpServer = http.createServer(app);
 dotenv.config();
+
+const MongodbStore = ConnectMongoDBSession(session)
+
+const store = new MongodbStore ({
+  uri: process.env.MONGO_URI,
+  collection: "sessions"
+})
+
+app.use(
+  session({
+    secret:process.env.SESSION_SECRETE,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly:true},
+    store:store  
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const server = new ApolloServer({
   typeDefs:mergedTypeDefs,
@@ -24,12 +51,15 @@ await server.start()
 
 app.use(
   '/',
-  cors(),
+  cors({
+    origin:"http://localhost:5173",
+    credentials:true
+  }),
   express.json(),
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req }) => buildContext({ req }),
   }),
 );
 
