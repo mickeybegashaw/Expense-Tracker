00094e90371256430ffc803483cc80d3ab505b9e
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-03-19 20:30:14 +0300

    Integrate Apollo Client for GraphQL data fetching in main application

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,21 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import GridBackground from "./components/ui/GridBackground.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+  credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <GridBackground>
-        <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
       </GridBackground>
     </BrowserRouter>
   </StrictMode>
